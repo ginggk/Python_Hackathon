aaa6232b069f7a46b53258ca8178907b64dccfb4
basecamp <jmorgan@basecampcodingacademy.org>
2018-11-12 16:21:22 -0600

    Added battle stage screen to be displayed when game state is battle

--- shell.py
+++ shell.py
@@ -24,55 +24,76 @@ import os
 
 
 def update(key, state: Game) -> Game:
-    if key == "KEY_UP":
-        state.clear_player_spot()
-        state.move_player_north()
-        state.player_new_spot()
-        return state
-    elif key == "KEY_DOWN":
-        state.clear_player_spot()
-        state.move_player_south()
-        state.player_new_spot()
-        return state
-    elif key == "KEY_LEFT":
-        state.clear_player_spot()
-        state.move_player_west()
-        state.player_new_spot()
-        return state
-    elif key == "KEY_RIGHT":
-        state.clear_player_spot()
-        state.move_player_east()
-        state.player_new_spot()
-        return state
-    else:
-        return state
+    if state.state == "explore":
+        if key == "KEY_UP":
+            state.clear_player_spot()
+            state.move_player_north()
+            state.player_new_spot()
+            return state
+        elif key == "KEY_DOWN":
+            state.clear_player_spot()
+            state.move_player_south()
+            state.player_new_spot()
+            return state
+        elif key == "KEY_LEFT":
+            state.clear_player_spot()
+            state.move_player_west()
+            state.player_new_spot()
+            return state
+        elif key == "KEY_RIGHT":
+            state.clear_player_spot()
+            state.move_player_east()
+            state.player_new_spot()
+            return state
+        else:
+            return state
+    # elif state.state = "battle":
+
+    return state
 
 
 def view(state, x, y):
-    string = f'Use the Arrow keys to move\nPlayer: {state.player.name}\n\n\t'
-    for row in state.player.room.build:
-        for cell in row:
-            if cell == 0:
-                string += "   "
-            elif cell == 1:
-                string += " P "
-            elif cell == 2:
-                string += "|W|"
-            elif cell == 3:
-                string += " E "
-            elif cell == 4:
-                string += " X "
-        string += "\n\t"
+    if state.state == "explore":
+        string = f'Use the Arrow keys to move\nPlayer: {state.player.name}\n\n\t'
+        for row in state.player.room.build:
+            for cell in row:
+                if cell == 0:
+                    string += "   "
+                elif cell == 1:
+                    string += " P "
+                elif cell == 2:
+                    string += "|W|"
+                elif cell == 3:
+                    string += " E "
+                elif cell == 4:
+                    string += " X "
+            string += "\n\t"
+    elif state.state == "battle":
+        player = state.player
+        enemy = player.room.enemy
+
+        string = f'\t|| {player.name} ||\t\t\t|| {enemy.name} ||\n\tHealth: {player.health}\t\t\tHealth: {enemy.health}'
+        return string
+
     return string
+
+
+def player_name():
+
+    name = input("What is your name: ")
+    if len(name) == 0:
+        player_name()
+    else:
+        return name
 
 
 def main():
     os.system('clear')
     _map = load_map()
-    name = input("What is your name: ")
+    name = player_name()
     player = Player(100, name, {'x': 2, 'y': 1}, _map[0])
 
-    run(Game(player, _map), update, view)
+    run(Game(player, _map, "battle"), update, view)
 
 
 if __name__ == "__main__":
